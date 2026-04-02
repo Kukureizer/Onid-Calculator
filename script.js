575546243ab6823f7e4d74b9c89b7e55795c063a
Kukureizer <lantvoitanton@gmail.com>
2026-04-02 21:48:34 +0200

    refactor calculator state handling and fix leading zero and backspace

--- script.js
+++ script.js
@@ -19,35 +19,32 @@ let operation = function(event){
 }
 
 
-let calculate = function(firstNUmber, operator, currentNumber){
-    
+let calculate = function(){
+    let result;
     if(operator === "plus"){
-       let result = parseFloat(firstNUmber) + parseFloat(currentNumber);
-        upDisplay.textContent = result;
-        firstNUmber = result;
-        currentNumber = ""; 
+        result = parseFloat(firstNUmber) + parseFloat(currentNumber);
     } else if( operator === "minus"){
-        let result = parseFloat(firstNUmber) - parseFloat(currentNumber);
-        upDisplay.textContent = result;
-        firstNUmber = result;
-        currentNumber = ""; 
+        result = parseFloat(firstNUmber) - parseFloat(currentNumber);
     } else if(operator === "divide"){
-        let result = parseFloat(firstNUmber) / parseFloat(currentNumber);
-        upDisplay.textContent = result;
-        firstNUmber = result;
-        currentNumber = ""; 
+        result = parseFloat(firstNUmber) / parseFloat(currentNumber);  
     } else if(operator === "multiply") {
-        let result = parseFloat(firstNUmber) * parseFloat(currentNumber);
-        upDisplay.textContent = result;
-        firstNUmber = result;
-        currentNumber = ""; 
+        result = parseFloat(firstNUmber) * parseFloat(currentNumber);  
     }
+    firstNUmber = result;
+    operator = null;
+    currentNumber = result;
+    display.textContent =result;
 }
 
 buttons.addEventListener("click", function(event){
     if(event.target.dataset.type === "number"){
-        currentNumber = currentNumber + event.target.dataset.value;
+        if(currentNumber === "0"){
+        currentNumber = event.target.dataset.value;
         display.textContent = currentNumber;
+        } else {
+            currentNumber = currentNumber + event.target.dataset.value;
+            display.textContent = currentNumber;
+        }
     } else if (event.target.dataset.type === "operator") {
         if(event.target.dataset.value === "plus")
         {
@@ -65,14 +62,16 @@ buttons.addEventListener("click", function(event){
         }
         else if(event.target.dataset.value === "equal")
         {
-            calculate(firstNUmber, operator, currentNumber);            
+            calculate();           
+        }
+    } else if (event.target.dataset.type === "manage"){
+        if (event.target.dataset.value === "backspace") {
+            currentNumber = currentNumber.slice(0, -1);
+            if(currentNumber === ""){
+                currentNumber = "0";
+            }
+            display.textContent = currentNumber;
         }
     }
 })
 
-// else if (event.target.dataset.type === "manage"){
-//         if (event.target.dataset.value === "backspace") {
-//             currentNumber = currentNumber.slice(0, -1);
-//             display = currentNumber;
-//         }
-//     }
